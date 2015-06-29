fb07d03ae9bf1479c6345b93311486c965fff6c4
Yu Xiang <xyfudan@gmail.com>
2015-06-29 15:31:44 -0700

    add kitti protocol

--- fast-rcnn/experiments/scripts/kitti_caffenet.sh
+++ fast-rcnn/experiments/scripts/kitti_caffenet.sh
@@ -10,13 +10,13 @@ exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./tools/train_net.py --gpu $1 \
-  --solver models/CaffeNet/solver.prototxt \
+  --solver models/CaffeNet/kitti/solver.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_train \
   --cfg experiments/cfgs/kitti.yml
 
 time ./tools/test_net.py --gpu $1 \
-  --def models/CaffeNet/test.prototxt \
+  --def models/CaffeNet/kitti/test.prototxt \
   --net output/kitti/kitti_train/caffenet_fast_rcnn_kitti_iter_40000.caffemodel \
   --imdb kitti_val \
   --cfg experiments/cfgs/kitti.yml
