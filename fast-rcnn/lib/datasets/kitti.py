f0404573c83c8752e8f811b1dd1145de6d9d156d
Yu Xiang <xyfudan@gmail.com>
2015-06-30 18:06:58 -0700

    first test

--- fast-rcnn/lib/datasets/kitti.py
+++ fast-rcnn/lib/datasets/kitti.py
@@ -182,6 +182,23 @@ class kitti(datasets.imdb):
 
         return self.create_roidb_from_box_list(box_list, gt_roidb)
 
+    def evaluate_detections(self, all_boxes, output_dir):
+        # for each image
+        for im_ind, index in enumerate(self.image_index):
+            filename = os.path.join(output_dir, index + '.txt')
+            print 'Writing KITTI results to file ' + filename
+            with open(filename, 'wt') as f:
+                # for each class
+                for cls_ind, cls in enumerate(self.classes):
+                    if cls == '__background__':
+                        continue
+                    dets = all_boxes[cls_ind][im_ind]
+                    if dets == []:
+                        continue
+                    for k in xrange(dets.shape[0]):
+                        f.write('{:s} -1 -1 -1 {:f} {:f} {:f} {:f} -1 -1 -1 -1 -1 -1 -1 {:f}\n'.format(\
+                                 cls, dets[k, 0], dets[k, 1], dets[k, 2], dets[k, 3], dets[k, -1]))
+
 if __name__ == '__main__':
     d = datasets.kitti('train')
     res = d.roidb
