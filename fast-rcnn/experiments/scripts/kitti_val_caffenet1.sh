5aee6c7fc19331e123114c6b1bccc51bcd0c53cf
Yu Xiang <xyfudan@gmail.com>
2015-07-17 16:37:31 -0700

    add 3 scales

--- fast-rcnn/experiments/scripts/kitti_val_caffenet1.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet1.sh
@@ -13,10 +13,10 @@ time ./tools/train_net.py --gpu $1 \
   --solver models/CaffeNet/kitti_val/solver1.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_train \
-  --cfg experiments/cfgs/kitti.yml
+  --cfg experiments/cfgs/kitti_3scales.yml
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_val/test1.prototxt \
   --net output/kitti/kitti_train/caffenet_fast_rcnn1_kitti_iter_40000.caffemodel \
   --imdb kitti_val \
-  --cfg experiments/cfgs/kitti.yml
+  --cfg experiments/cfgs/kitti_3scales.yml
