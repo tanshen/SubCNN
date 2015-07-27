233a7a8bc07b4734fb4cd666fce7cfceb39d6900
Yu Xiang <xyfudan@gmail.com>
2015-07-27 10:25:52 -0700

    kitti test 5 scales

--- fast-rcnn/experiments/scripts/kitti_test_caffenet.sh
+++ fast-rcnn/experiments/scripts/kitti_test_caffenet.sh
@@ -13,10 +13,11 @@ time ./tools/train_net.py --gpu $1 \
   --solver models/CaffeNet/kitti_test/solver.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_trainval \
-  --cfg experiments/cfgs/kitti_4scales.yml
+  --cfg experiments/cfgs/kitti_5scales.yml \
+  --iters 60000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_test/test.prototxt \
-  --net output/kitti/kitti_trainval/caffenet_fast_rcnn_kitti_iter_40000.caffemodel \
+  --net output/kitti/kitti_trainval/caffenet_fast_rcnn_kitti_iter_60000.caffemodel \
   --imdb kitti_test \
-  --cfg experiments/cfgs/kitti_4scales.yml
+  --cfg experiments/cfgs/kitti_5scales.yml
