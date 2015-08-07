9a33d0cecd01b101026690499a9c0eb7cf659237
Yu Xiang <xyfudan@gmail.com>
2015-08-06 17:58:26 -0700

    start roi generating layer

---
+++ fast-rcnn/lib/roi_generating_layer/layer.py
@@ -0,0 +1,81 @@
+# --------------------------------------------------------
+# Copyright (c) 2015 Stanford CVGL
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Yu Xiang
+# --------------------------------------------------------
+
+"""The layer used during training to train a Fast R-CNN network.
+
+RoIGeneratingLayer implements a Caffe Python layer.
+"""
+
+import caffe
+from fast_rcnn.config import cfg
+import numpy as np
+import yaml
+from multiprocessing import Process, Queue
+
+class RoIGeneratingLayer(caffe.Layer):
+    """Fast R-CNN data layer used for training."""
+
+    def setup(self, bottom, top):
+        """Setup the RoIDataLayer."""
+
+        # parse the layer parameter string, which must be valid YAML
+        layer_params = yaml.load(self.param_str_)
+
+        self._num_classes = layer_params['num_classes']
+
+        self._name_to_top_map = {
+            'data': 0,
+            'rois': 1,
+            'labels': 2}
+
+        # data blob: holds a batch of N images, each with 3 channels
+        # The height and width (100 x 100) are dummy values
+        top[0].reshape(1, 3, 100, 100)
+
+        # rois blob: holds R regions of interest, each is a 5-tuple
+        # (n, x1, y1, x2, y2) specifying an image batch index n and a
+        # rectangle (x1, y1, x2, y2)
+        top[1].reshape(1, 5)
+
+        # labels blob: R categorical labels in [0, ..., K] for K foreground
+        # classes plus background
+        top[2].reshape(1)
+
+        if cfg.TRAIN.BBOX_REG:
+            self._name_to_top_map['bbox_targets'] = 3
+            self._name_to_top_map['bbox_loss_weights'] = 4
+
+            # bbox_targets blob: R bounding-box regression targets with 4
+            # targets per class
+            top[3].reshape(1, self._num_classes * 4)
+
+            # bbox_loss_weights blob: At most 4 targets per roi are active;
+            # thisbinary vector sepcifies the subset of active targets
+            top[4].reshape(1, self._num_classes * 4)
+
+        # add subclass labels
+        if cfg.TRAIN.SUBCLS:
+            self._name_to_top_map['sublabels'] = 5
+            top[5].reshape(1)
+            
+    def forward(self, bottom, top):
+        """Get blobs and copy them into this layer's top blob vector."""
+        blobs = self._get_next_minibatch()
+
+        for blob_name, blob in blobs.iteritems():
+            top_ind = self._name_to_top_map[blob_name]
+            # Reshape net's input blobs
+            top[top_ind].reshape(*(blob.shape))
+            # Copy data into net's input blobs
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
