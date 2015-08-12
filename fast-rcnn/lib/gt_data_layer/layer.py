b0153df941d41b1eb80629447b0adab022298414
Yu Xiang <xyfudan@gmail.com>
2015-08-11 17:59:20 -0700

    start gt layer

---
+++ fast-rcnn/lib/gt_data_layer/layer.py
@@ -0,0 +1,97 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""The data layer used during training to train a Fast R-CNN network.
+
+GtDataLayer implements a Caffe Python layer.
+"""
+
+import caffe
+from fast_rcnn.config import cfg
+from gt_data_layer.minibatch import get_minibatch
+import numpy as np
+import yaml
+from multiprocessing import Process, Queue
+
+class GtDataLayer(caffe.Layer):
+    """Fast R-CNN data layer used for training."""
+
+    def _shuffle_roidb_inds(self):
+        """Randomly permute the training roidb."""
+        self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds()
+
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+        self._cur += cfg.TRAIN.IMS_PER_BATCH
+        return db_inds
+
+    def _get_next_minibatch(self):
+        """Return the blobs to be used for the next minibatch."""
+        db_inds = self._get_next_minibatch_inds()
+        minibatch_db = [self._roidb[i] for i in db_inds]
+        return get_minibatch(minibatch_db, self._num_classes)
+
+    # this function is called in training the net
+    def set_roidb(self, roidb):
+        """Set the roidb to be used by this layer during training."""
+        self._roidb = roidb
+        self._shuffle_roidb_inds()
+
+    def setup(self, bottom, top):
+        """Setup the GtDataLayer."""
+
+        # parse the layer parameter string, which must be valid YAML
+        layer_params = yaml.load(self.param_str_)
+
+        self._num_classes = layer_params['num_classes']
+
+        self._name_to_top_map = {
+            'data': 0,
+            'rois': 1,
+            'labels': 2}
+
+        # data blob: holds a batch of N images, each with 3 channels
+        # The height and width (100 x 100) are dummy values
+        top[0].reshape(1, 3, 100, 100)
+
+        # rois blob: holds R regions of interest, each is a 5-tuple
+        # (n, x1, y1, x2, y2) specifying an image batch index n and a
+        # rectangle (x1, y1, x2, y2)
+        top[1].reshape(1, 5)
+
+        # labels blob: R categorical labels in [0, ..., K] for K foreground
+        # classes plus background
+        top[2].reshape(1)
+
+        # add subclass labels
+        if cfg.TRAIN.SUBCLS:
+            self._name_to_top_map['sublabels'] = 3
+            top[3].reshape(1)
+            
+    def forward(self, bottom, top):
+        """Get blobs and copy them into this layer's top blob vector."""
+        blobs = self._get_next_minibatch()
+
+        for blob_name, blob in blobs.iteritems():
+            top_ind = self._name_to_top_map[blob_name]
+            # Reshape net's input blobs
+            top[top_ind].reshape(*(blob.shape))
+            # Copy data into net's input blobs
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
