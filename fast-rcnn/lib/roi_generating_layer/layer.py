e27e75b0b970db80a3e5cf52dbe3700986388287
Yu Xiang <yuxiang@napoli108.Stanford.EDU>
2015-08-19 20:51:09 -0700

    zero roi gradient

--- fast-rcnn/lib/roi_generating_layer/layer.py
+++ fast-rcnn/lib/roi_generating_layer/layer.py
@@ -310,7 +310,8 @@ class RoIGeneratingLayer(caffe.Layer):
 
     def backward(self, top, propagate_down, bottom):
         """This layer does not propagate gradients."""
-        pass
+        # Initialize all the gradients to 0. We will accumulate gradient
+        bottom[0].diff[...] = np.zeros_like(bottom[0].data)
 
     def reshape(self, bottom, top):
         """Reshaping happens during the call to forward."""
