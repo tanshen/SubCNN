d4ab3fa5abf7ee4b33ad93f308373ad9019bac82
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-09-07 17:14:05 -0700

    roi proposal training only

---
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_roi_proposal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/kitti_val_caffenet_roi_proposal.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/train_net.py --gpu $1 \
+  --solver models/CaffeNet/kitti_val/solver_roi_proposal.prototxt \
+  --weights data/imagenet_models/CaffeNet.v2.caffemodel \
+  --imdb kitti_train \
+  --cfg experiments/cfgs/kitti_multiscales.yml
