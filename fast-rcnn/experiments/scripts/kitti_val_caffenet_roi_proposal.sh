9f098196e508b4f9747f039cf764a860ad0b44ec
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-09-08 23:50:18 -0700

    test proposal training

--- fast-rcnn/experiments/scripts/kitti_val_caffenet_roi_proposal.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_roi_proposal.sh
@@ -14,3 +14,9 @@ time ./tools/train_net.py --gpu $1 \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_train \
   --cfg experiments/cfgs/kitti_multiscales_proposal.yml
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/CaffeNet/kitti_val/test_roi_proposal.prototxt \
+  --net output/kitti/kitti_train/caffenet_fast_rcnn_roi_kitti_iter_10000.caffemodel \
+  --imdb kitti_val \
+  --cfg experiments/cfgs/kitti_multiscales_proposal.yml
