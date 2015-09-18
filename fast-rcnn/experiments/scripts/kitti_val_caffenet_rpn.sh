2bcb9097fc2e6dde4b89edcfa63fd3a256b0adee
Yu Xiang <yuxiang@napoli106.Stanford.EDU>
2015-09-18 11:17:10 -0700

    iters 20000 for rpn

--- fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn.sh
@@ -13,7 +13,7 @@ time ./tools/train_net.py --gpu $1 \
   --solver models/CaffeNet/kitti_val/solver_rpn.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_train \
-  --cfg experiments/cfgs/kitti_rpn.yml
+  --cfg experiments/cfgs/kitti_rpn.yml \
   --iters 20000
 
 time ./tools/test_net.py --gpu $1 \
