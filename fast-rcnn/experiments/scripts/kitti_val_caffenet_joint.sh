e31c5f38db3ad3fe747fa0eab3986a7cb35d5fa7
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-09-24 18:01:30 -0700

    use cfg joint

--- fast-rcnn/experiments/scripts/kitti_val_caffenet_joint.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_joint.sh
@@ -13,11 +13,11 @@ time ./tools/train_net.py --gpu $1 \
   --solver models/CaffeNet/kitti_val/solver_joint.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb kitti_train \
-  --cfg experiments/cfgs/kitti_rpn.yml \
+  --cfg experiments/cfgs/kitti_joint.yml \
   --iters 40000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_val/test_joint.prototxt \
   --net output/kitti/kitti_train/caffenet_fast_rcnn_joint_kitti_iter_40000.caffemodel \
   --imdb kitti_val \
-  --cfg experiments/cfgs/kitti_rpn.yml
+  --cfg experiments/cfgs/kitti_joint.yml
