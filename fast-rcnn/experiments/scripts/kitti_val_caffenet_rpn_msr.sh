2363f5128628a7eb70aa3a2eea7000525a34fed0
Yu Xiang <yuxiang@napoli109.Stanford.EDU>
2015-10-13 12:32:56 -0700

    tmp

--- fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn_msr.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn_msr.sh
@@ -9,12 +9,12 @@ LOG="experiments/logs/kitti_val_caffenet_rpn_msr.txt.`date +'%Y-%m-%d_%H-%M-%S'`
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu $1 \
-  --solver models/CaffeNet/kitti_val/solver_rpn_msr.prototxt \
-  --weights data/imagenet_models/CaffeNet.v2.caffemodel \
-  --imdb kitti_train \
-  --cfg experiments/cfgs/kitti_rpn_msr.yml \
-  --iters 40000
+#time ./tools/train_net.py --gpu $1 \
+#  --solver models/CaffeNet/kitti_val/solver_rpn_msr.prototxt \
+#  --weights data/imagenet_models/CaffeNet.v2.caffemodel \
+#  --imdb kitti_train \
+#  --cfg experiments/cfgs/kitti_rpn_msr.yml \
+#  --iters 40000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_val/test_rpn_msr.prototxt \
