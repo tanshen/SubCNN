86bb4e02191855a24deb4f186af1db40f76dc28a
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-10-17 15:29:31 -0700

    add google net

---
+++ fast-rcnn/experiments/scripts/kitti_val_googlenet_rpn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/kitti_val_caffenet_rpn.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/train_net.py --gpu $1 \
+  --solver models/GoogleNet/kitti_val/solver_rpn.prototxt \
+  --weights data/imagenet_models/bvlc_googlenet.caffemodel \
+  --imdb kitti_train \
+  --cfg experiments/cfgs/kitti_rpn.yml \
+  --iters 40000
+
+#time ./tools/test_net.py --gpu $1 \
+#  --def models/CaffeNet/kitti_val/test_rpn.prototxt \
+#  --net output/kitti/kitti_train/caffenet_fast_rcnn_rpn_kitti_iter_40000.caffemodel \
+#  --imdb kitti_val \
+#  --cfg experiments/cfgs/kitti_rpn.yml
+
+#time ./tools/test_net.py --gpu $1 \
+#  --def models/CaffeNet/kitti_val/test_rpn.prototxt \
+#  --net output/kitti/kitti_train/caffenet_fast_rcnn_rpn_kitti_iter_40000.caffemodel \
+#  --imdb kitti_train \
+#  --cfg experiments/cfgs/kitti_rpn.yml
