c966965ba2b45e9f26ee67ccaf853156341a6d6d
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-10-20 13:57:02 -0700

    add one conv layer to rpn fix

--- fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn_fix.sh
+++ fast-rcnn/experiments/scripts/kitti_val_caffenet_rpn_fix.sh
@@ -9,12 +9,12 @@ LOG="experiments/logs/kitti_val_caffenet_rpn_fix.txt.`date +'%Y-%m-%d_%H-%M-%S'`
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-#time ./tools/train_net.py --gpu $1 \
-#  --solver models/CaffeNet/kitti_val/solver_rpn_fix.prototxt \
-#  --weights output/kitti/kitti_train/caffenet_fast_rcnn_kitti_iter_40000.caffemodel \
-#  --imdb kitti_train \
-#  --cfg experiments/cfgs/kitti_rpn_fix.yml \
-#  --iters 40000
+time ./tools/train_net.py --gpu $1 \
+  --solver models/CaffeNet/kitti_val/solver_rpn_fix.prototxt \
+  --weights output/kitti/kitti_train/caffenet_fast_rcnn_kitti_iter_40000.caffemodel \
+  --imdb kitti_train \
+  --cfg experiments/cfgs/kitti_rpn_fix.yml \
+  --iters 40000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_val/test_rpn_fix.prototxt \
