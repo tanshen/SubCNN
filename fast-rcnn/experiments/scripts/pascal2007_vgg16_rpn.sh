7f5c66b4d134444ee61bca779218db859a35d5df
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-10-31 18:37:19 -0700

    add vgg16 pascal rpn

---
+++ fast-rcnn/experiments/scripts/pascal2007_vgg16_rpn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/pascal2007_vgg16_rpn.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/train_net.py --gpu $1 \
+  --solver models/VGG16/pascal2007/solver_rpn.prototxt \
+  --weights data/imagenet_models/VGG16.v2.caffemodel \
+  --imdb voc_2007_trainval \
+  --cfg experiments/cfgs/pascal_rpn.yml \
+  --iters 80000
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/VGG16/pascal2007/test_rpn.prototxt \
+  --net output/pascal2007/voc_2007_trainval/vgg16_fast_rcnn_rpn_pascal2007_iter_80000.caffemodel \
+  --imdb voc_2007_trainval \
+  --cfg experiments/cfgs/pascal_rpn.yml
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/VGG16/pascal2007/test_rpn.prototxt \
+  --net output/pascal2007/voc_2007_trainval/vgg16_fast_rcnn_rpn_pascal2007_iter_80000.caffemodel \
+  --imdb voc_2007_test \
+  --cfg experiments/cfgs/pascal_rpn.yml
