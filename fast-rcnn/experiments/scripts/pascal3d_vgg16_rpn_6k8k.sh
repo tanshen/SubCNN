7a7338bad9f9097ae5b7b060f9d71a2f641ced64
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-11-02 15:33:56 -0800

    add vgg rpn 6k8k

--- fast-rcnn/experiments/scripts/pascal3d_vgg16_rpn_6k8k.sh
+++ fast-rcnn/experiments/scripts/pascal3d_vgg16_rpn_6k8k.sh
@@ -5,25 +5,25 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-LOG="experiments/logs/pascal3d_vgg16_rpn.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/pascal3d_vgg16_rpn_6k8k.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./tools/train_net.py --gpu $1 \
-  --solver models/VGG16/pascal3d/solver_rpn.prototxt \
+  --solver models/VGG16/pascal3d/solver_rpn_6k8k.prototxt \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
   --imdb pascal3d_train \
   --cfg experiments/cfgs/pascal3d_rpn_vgg16.yml \
-  --iters 40000
+  --iters 80000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/VGG16/pascal3d/test_rpn.prototxt \
-  --net output/pascal3d/pascal3d_train/vgg16_fast_rcnn_rpn_pascal3d_iter_40000.caffemodel \
+  --net output/pascal3d/pascal3d_train/vgg16_fast_rcnn_rpn_6k8k_pascal3d_iter_80000.caffemodel \
   --imdb pascal3d_train \
   --cfg experiments/cfgs/pascal3d_rpn_vgg16.yml
 
 time ./tools/test_net.py --gpu $1 \
   --def models/VGG16/pascal3d/test_rpn.prototxt \
-  --net output/pascal3d/pascal3d_train/vgg16_fast_rcnn_rpn_pascal3d_iter_40000.caffemodel \
+  --net output/pascal3d/pascal3d_train/vgg16_fast_rcnn_rpn_6k8k_pascal3d_iter_80000.caffemodel \
   --imdb pascal3d_val \
   --cfg experiments/cfgs/pascal3d_rpn_vgg16.yml
