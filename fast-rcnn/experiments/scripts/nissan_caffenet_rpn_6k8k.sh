b93525ebf15f67f8d1270a4afba4451655f105b3
Yu Xiang <yuxiang@napoli109.Stanford.EDU>
2015-12-31 18:16:42 -0800

    add nissan cfg

--- fast-rcnn/experiments/scripts/nissan_caffenet_rpn_6k8k.sh
+++ fast-rcnn/experiments/scripts/nissan_caffenet_rpn_6k8k.sh
@@ -13,4 +13,4 @@ time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/kitti_test/test_rpn.prototxt \
   --net output/kitti/kitti_trainval/caffenet_fast_rcnn_rpn_6k8k_kitti_iter_80000.caffemodel \
   --imdb nissan_2015-10-21-16-25-12 \
-  --cfg experiments/cfgs/kitti_rpn.yml
+  --cfg experiments/cfgs/nissan_rpn.yml
