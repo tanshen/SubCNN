ed60577230ba42abf708e272125aa937feb73aa7
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2016-02-26 01:21:02 -0800

    more iters imagenet3d rcnn

--- fast-rcnn/experiments/scripts/imagenet3d_caffenet_rcnn_original_edge_boxes.sh
+++ fast-rcnn/experiments/scripts/imagenet3d_caffenet_rcnn_original_edge_boxes.sh
@@ -5,7 +5,7 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-LOG="experiments/logs/imagenet3d_caffenet_rcnn_original_selective_search.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/imagenet3d_caffenet_rcnn_original_edge_boxes.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
@@ -13,11 +13,11 @@ time ./tools/train_net.py --gpu $1 \
   --solver models/CaffeNet/imagenet3d/solver_rcnn_original.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb imagenet3d_trainval \
-  --cfg experiments/cfgs/imagenet3d_rcnn_original_selective_search.yml \
+  --cfg experiments/cfgs/imagenet3d_rcnn_original_edge_boxes.yml \
   --iters 160000
 
 time ./tools/test_net.py --gpu $1 \
   --def models/CaffeNet/imagenet3d/test_rcnn_original.prototxt \
-  --net output/imagenet3d/imagenet3d_trainval/caffenet_fast_rcnn_original_imagenet3d_selective_search_iter_160000.caffemodel \
+  --net output/imagenet3d/imagenet3d_trainval/caffenet_fast_rcnn_original_imagenet3d_edge_boxes_iter_160000.caffemodel \
   --imdb imagenet3d_test \
-  --cfg experiments/cfgs/imagenet3d_rcnn_original_selective_search.yml
+  --cfg experiments/cfgs/imagenet3d_rcnn_original_edge_boxes.yml
