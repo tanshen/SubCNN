92654b17765ac084956486b8f1c3e20df154defa
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2016-02-27 01:48:28 -0800

    add vgg rpn test imagenet3d

---
+++ fast-rcnn/experiments/scripts/imagenet3d_vgg16_rpn_msr_test_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/imagenet3d_vgg16_rpn_msr_test_2.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/VGG16/imagenet3d/test_rpn_msr.prototxt \
+  --net output/imagenet3d/imagenet3d_trainval/vgg16_fast_rcnn_rpn_msr_imagenet3d_iter_160000.caffemodel \
+  --imdb imagenet3d_test_2 \
+  --cfg experiments/cfgs/imagenet3d_rpn_msr.yml
