c37eb291b79bbf86e7a9a6c45cc9346db40d9886
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2016-04-09 21:49:56 -0700

    kitti tracking val test

---
+++ fast-rcnn/experiments/scripts/kitti_tracking_val_caffenet_rpn_test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+LOG="experiments/logs/kitti_tracking_val_caffenet_rpn_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+image_set="training"
+
+for i in {0..20}
+do
+
+seq_num=$(printf '%04d' "$i")
+echo $seq_num
+
+time ./tools/test_net.py --gpu $1 \
+  --def models/CaffeNet/kitti_tracking_val/test_rpn.prototxt \
+  --net output/kitti/kitti_tracking_training_train/caffenet_fast_rcnn_rpn_kitti_iter_40000.caffemodel \
+  --imdb kitti_tracking_$image_set\_$seq_num \
+  --cfg experiments/cfgs/kitti_tracking_rpn.yml
+
+# create an symbol link for the region proposal results
+ln -s $PWD/output/kitti_tracking/kitti_tracking_$image_set\_$seq_num/caffenet_fast_rcnn_rpn_kitti_iter_40000 data/KITTI_Tracking/region_proposals/RPN_train/$image_set/$seq_num
+
+done
