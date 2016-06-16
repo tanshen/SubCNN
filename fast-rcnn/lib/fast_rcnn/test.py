fb6a77e68483bcb0e59ab10ad0c75a72c4868701
Yu Xiang <yuxiang@capri5.stanford.edu>
2016-06-16 16:09:13 -0700

    do not show proposal box

--- fast-rcnn/lib/fast_rcnn/test.py
+++ fast-rcnn/lib/fast_rcnn/test.py
@@ -448,7 +448,7 @@ def im_detect_proposal(net, im, boxes_grid, num_classes, num_subclasses, subclas
     print scores.shape
    
     # draw boxes
-    if 1:
+    if 0:
         # print scores, pred_boxes.shape
         import matplotlib.pyplot as plt
         plt.imshow(im)
