4b5b4eb003c9e2fa1baadb9bcb6f5a8ed6f2d776
Yu Xiang <yuxiang@capri5.stanford.edu>
2016-06-21 14:01:44 -0700

    fix a bug kitti tracking test script

--- fast-rcnn/experiments/scripts/kitti_tracking_test_vgg16_rcnn_multiscale.sh
+++ fast-rcnn/experiments/scripts/kitti_tracking_test_vgg16_rcnn_multiscale.sh
@@ -9,12 +9,12 @@ LOG="experiments/logs/kitti_tracking_test_vgg16_rcnn_multiscale.txt.`date +'%Y-%
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu $1 \
-  --solver models/VGG16/kitti_tracking_test/solver_rcnn_multiscale.prototxt \
-  --weights data/imagenet_models/VGG16.v2.caffemodel \
-  --imdb kitti_tracking_training_trainval \
-  --cfg experiments/cfgs/kitti_tracking_multiscale_vgg16.yml \
-  --iters 80000
+#time ./tools/train_net.py --gpu $1 \
+#  --solver models/VGG16/kitti_tracking_test/solver_rcnn_multiscale.prototxt \
+#  --weights data/imagenet_models/VGG16.v2.caffemodel \
+#  --imdb kitti_tracking_training_trainval \
+#  --cfg experiments/cfgs/kitti_tracking_multiscale_vgg16.yml \
+#  --iters 80000
 
 image_set="testing"
 
@@ -32,6 +32,8 @@ time ./tools/test_net.py --gpu $1 \
 
 # copy the detection result
 cp output/kitti_tracking/kitti_tracking_$image_set\_$seq_num/vgg16_fast_rcnn_multiscale_trainval_kitti_iter_80000/$seq_num.txt data/KITTI_Tracking/detection_trainval_vgg16/$image_set
+
+done
 
 image_set="training"
 
